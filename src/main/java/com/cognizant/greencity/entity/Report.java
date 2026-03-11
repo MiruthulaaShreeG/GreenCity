aabfe12543a13b0017e1b7b49d22fab486816b8f
G <2478283@cognizant.com>
2026-03-11 16:09:54 +0530

    added template

---
+++ src/main/java/com/cognizant/greencity/entity/Report.java
@@ -0,0 +1,4 @@
+package com.cognizant.greencity.entity;
+
+public class Report {
+}
---
+++ src/test/java/com/cognizant/greencity/GreencityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.greencity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreencityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
