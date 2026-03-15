625e20e3791b74fc5c90e8baa6f9b525ac7e3e5b
Aman <Aman.Kumar20c447@cognizant.com>
2026-03-16 00:08:42 +0530

    written service logic for user Entity

--- src/main/java/com/cognizant/greencity/dto/UserDTO.java
+++ src/main/java/com/cognizant/greencity/dto/UserDTO.java
@@ -1,5 +1,80 @@
 package com.cognizant.greencity.dto;
 
 public class UserDTO {
+private Integer userId;
+private String name;
+private String email;
+private String phone;
+private String role;
+private String password;
+private String confirmPassword;
+private String status;
 
+public UserDTO(){
+
+}
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
