c556e9065a552e9ca8156fca0dfd217a0ffd0569
G <2478283@cognizant.com>
2026-03-16 11:19:11 +0530

    changes commited

---
+++ src/main/java/com/cognizant/greencity/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cognizant.greencity.dao;
+import com.cognizant.greencity.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer>{
+    User findByEmail(String email);
+}
