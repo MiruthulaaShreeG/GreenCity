349a19ec16d5aaa3605a8a768b918c5f10cd29ca
Balasubramanian-442 <Balasubramanian.Easwaran@cognizant.com>
2026-03-24 18:39:47 +0530

    added files

--- src/main/java/com/cognizant/greencity/entity/Resource.java
+++ src/main/java/com/cognizant/greencity/entity/Resource.java
@@ -1,14 +1,12 @@
 package com.cognizant.greencity.entity;
 import jakarta.persistence.*;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
-
 @Entity
-@Table(name = "Resource")
+@Table(name = "resource")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,30 +14,28 @@ public class Resource {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ResourceID", updatable = false, nullable = false)
+    @Column(name = "resourceid", updatable = false, nullable = false)
     private Integer resourceId;
 
-    @Column(name = "Type", nullable = false, length = 50)
+    @Column(name = "type", nullable = false, length = 50)
     private String type; // Energy / Water / Waste
 
-    @Column(name = "Location", nullable = false, length = 255)
+    @Column(name = "location", nullable = false, length = 255)
     private String location;
 
-    @Column(name = "Capacity", nullable = false)
+    @Column(name = "capacity", nullable = false)
     private Double capacity;
 
-    @Column(name = "Status", nullable = false, length = 50)
+    @Column(name = "status", nullable = false, length = 50)
     private String status;
 
     // One resource can have multiple usage records
     @OneToMany(mappedBy = "resource", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ResourceUsage> usages;
 
-    @ManyToOne(optional=true)
-    @JoinColumn(name = "project_id", nullable = true)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
     private Project project;
-
-    // Getters and Setters
 
 }
 
